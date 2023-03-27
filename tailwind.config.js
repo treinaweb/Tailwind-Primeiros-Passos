54f24b69520a98c75746e4d0894a62cb9e7a013e
RafaelOliveira013 <rafaeletec013@gmail.com>
2023-03-27 14:54:51 -0300

    01:16 Adicionando fontes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'inter': ['Inter', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
